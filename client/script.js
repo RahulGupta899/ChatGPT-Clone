dc42e185af076c017953449e6724891244df128c
Rahul Gupta <FKOL015526@fusionbposervices.com>
2023-02-12 12:33:44 +0530

    Initial Code

---
+++ client/script.js
@@ -0,0 +1,115 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+///////////////////////////
+// CREATING A LOADER
+///////////////////////////
+let loadInterval;
+function loader(element){
+  element.textContent = ' ';
+  loadInterval = setInterval(()=>{
+    element.textContent += '.';
+    if(element.textContent === ' ....'){  //WHEN REACED 4 DOTS MAKE 1
+      element.textContent = ' ';
+    }
+  },300)
+}
+
+//////////////////////////////////////////
+// LINE BY LINE TEXT PRINTING ANIMATION
+//////////////////////////////////////////
+function typeText(element,text){
+  let index = 0;
+  let interval = setInterval(()=>{
+    if(index <  text.length){
+      element.innerHTML += text.charAt(index)
+      index++
+      element.scrollIntoView()
+    }
+    else{
+       clearInterval(interval)
+    }
+  },20)
+}
+
+//////////////////////////////////////////////
+// GENERATE RANDOM UNIQUE IDs FOR EACH ANSWER
+/////////////////////////////////////////////
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`
+}
+
+////////////////////////
+// Each Conversation
+////////////////////////
+function chatStripe(isAI, value, uniqueId){
+  return(
+    `
+      <div class="wrapper ${isAI && 'ai'}">
+        <div class='chat'>
+          <div className='profile'>
+            <img
+              src="${isAI ? bot : user}"
+              alt="${isAI ? bot : user}"
+            />
+          </div>
+          <div class='message' id=${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+
+const handleSubmit = async(e)=>{
+  e.preventDefault();
+  const data = new FormData(form);
+  
+  // User's chatStripe
+  chatContainer.innerHTML += chatStripe(false,data.get('prompt'),generateUniqueId());
+  
+  form.reset()    // Clear the Text Area Input
+
+  // AI's ChatStripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true," ",uniqueId)
+  chatContainer.scrollTop = chatContainer.scrollHeight;  // This will create the new message in view
+  const messageDiv = document.getElementById(uniqueId)
+  loader(messageDiv)
+  
+  const response = await fetch('http://localhost:5000',{
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+  
+  clearInterval(loadInterval)
+  messageDiv.innerHTMl = '';
+  
+  if(response.ok){
+    const data = await response.json();
+    const parseData = data.bot.trim();
+    typeText(messageDiv, parseData);
+  }
+  else{
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong..."
+  }
+}
+
+//////////////////////
+// EVENT LISTENERS
+/////////////////////
+form.addEventListener('submit',handleSubmit)
+form.addEventListener('keyup',(e)=>{
+  if(e.keyCode === 13) handleSubmit(e)
+})
---
+++ server/server.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import {Configuration,OpenAIApi } from 'openai'
+dotenv.config()
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+const opeai = new OpenAIApi(configuration)
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async(req,res)=>{
+    res.status(200).send({
+        message: 'Hello from codeX'
+    })
+})
+
+app.post('/',async(req,res)=>{
+    try{
+        const prompt = req.body.prompt;
+        const response = await opeai.createCompletion({
+            model: 'code-davinci-002',
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        }) 
+        res.json({
+            bot: response.data.choices[0].text
+        }) 
+    }
+    catch(error){
+        console.log(error.message)
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+})
+
+app.listen(5000, ()=>{
+    console.log('SERVER IS RUNNING ON PORT http://localhost:5000')
+})
+
